0594b4d506300e06c72195a5853986668fef834f
Grigory Markin <gr.markin@gmail.com>
2012-10-04 21:13:39 +0200

    The prefix of the template project modified

---
+++ org.limepepper.templateproject.ui/src/org/limepepper/templateproject/ui/wizards/NewTemplateProjectWizardPage1.java
@@ -0,0 +1,21 @@
+package org.limepepper.templateproject.ui.wizards;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+public class NewTemplateProjectWizardPage1 extends WizardPage{
+
+	protected NewTemplateProjectWizardPage1(String pageName) {
+		super(pageName);
+		this.setTitle("New Template Project");
+		this.setDescription(pageName);
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NO_FOCUS);
+		this.setControl(container);		
+	}
+
+}
---
+++ org.limepepper.templateproject.ui/src/org/limepepper/templateproject/ui/wizards/NewTemplateProjectWizardPageConfirm.java
@@ -0,0 +1,12 @@
+package org.limepepper.templateproject.ui.wizards;
+
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+public class NewTemplateProjectWizardPageConfirm extends WizardNewProjectCreationPage{
+
+	public NewTemplateProjectWizardPageConfirm(String pageName) {
+		super(pageName);
+		this.setTitle(pageName);
+	}
+
+}
