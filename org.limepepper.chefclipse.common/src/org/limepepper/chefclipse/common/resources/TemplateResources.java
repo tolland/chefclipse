b3977d2d01d2ad2662f034cd978a2a04ca597f4f
Grigory Markin <gr.markin@gmail.com>
2012-10-05 18:38:24 +0200

    Build enabled

---
+++ org.limepepper.chefclipse.common/src/org/limepepper/chefclipse/common/resources/TemplateResources.java
@@ -0,0 +1,71 @@
+package org.limepepper.chefclipse.common.resources;
+
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+/*
+ * Helper routines to access and create resources with respect to structure
+ * of the template project.
+ */
+public class TemplateResources {
+	
+	public static String MODULES_FOLDER_NAME = ".modules";
+	public static String PROJECT_NATURE_ID = "org.limepepper.rcp.chefclipse.common.resources.chefclipseNature";
+	
+	public static Object[] getProjects(IWorkspaceRoot root) {		
+		Object[] projects = new Object[root.getProjects().length];				
+		for(int i = 0; i < root.getProjects().length; i++){
+			Object proj = getProject(root.getProjects()[i]);
+			if(proj != null)
+				projects[i] = proj;
+		}
+		
+		return projects;
+	}
+	
+	public static TemplateProject getProject(IProject project) {				
+		IProjectNature nature = null;
+		
+		try {
+			nature = project.getNature(PROJECT_NATURE_ID);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(nature != null){
+			return new TemplateProject(project);
+		}
+		
+		return null;
+	}	
+	
+	
+	public static void createchefclipse(IProject proj, IProgressMonitor monitor) throws CoreException{
+		IProjectDescription desc = 
+				proj.getWorkspace().newProjectDescription(proj.getName());
+		
+		desc.setNatureIds(new String[]{PROJECT_NATURE_ID});
+		
+		proj.create(desc, monitor);
+	}
+	
+	
+	public static void createTemplateModule(String moduleName, IProject project, IProgressMonitor monitor) throws CoreException{
+		IFolder modulesFolder = project.getFolder(MODULES_FOLDER_NAME);
+		
+		if(!modulesFolder.exists()){
+			modulesFolder.create(true, true, monitor);
+		}
+		
+		IFolder module = modulesFolder.getFolder(moduleName);
+		module.create(true, true, monitor);
+	}
+	
+}
---
+++ org.limepepper.chefclipse.ui/src/org/limepepper/chefclipse/ui/wizards/NewModuleWizardPageConfirm.java
@@ -0,0 +1,200 @@
+package org.limepepper.chefclipse.ui.wizards;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+import org.limepepper.chefclipse.common.resources.TemplateProject;
+import org.limepepper.chefclipse.common.resources.TemplateResources;
+
+
+
+public class NewModuleWizardPageConfirm extends WizardPage {
+	private Text containerText;
+	private Text moduleText;
+
+	private ISelection selection;
+
+	
+	public NewModuleWizardPageConfirm(ISelection selection) {
+		super("moduleWizardPage");
+		setTitle("New Template Module");
+		setDescription("This wizard creates a new module");
+		this.selection = selection;
+	}
+
+	
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 3;
+		layout.verticalSpacing = 9;
+		Label label = new Label(container, SWT.NULL);
+		label.setText("&Container:");
+
+		containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		containerText.setLayoutData(gd);
+		containerText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+
+		Button button = new Button(container, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowse();
+			}
+		});
+		label = new Label(container, SWT.NULL);
+		label.setText("&Module name:");
+
+		moduleText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		moduleText.setLayoutData(gd);
+		moduleText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+		initialize();
+		dialogChanged();
+		setControl(container);
+	}
+
+	/**
+	 * Tests if the current workbench selection is a suitable container to use.
+	 */
+
+	private void initialize() {
+		if (selection != null && selection.isEmpty() == false
+				&& selection instanceof IStructuredSelection) {
+			
+			IStructuredSelection ssel = (IStructuredSelection) selection;
+			if (ssel.size() > 1)
+				return;
+			
+			Object obj = ssel.getFirstElement();
+			if (obj instanceof IResource) {
+				IContainer container;
+				if (obj instanceof IContainer)
+					container = (IContainer) obj;
+				else
+					container = ((IResource) obj).getParent();
+				
+				// Check if the container is the template project
+				if(container instanceof IProject){
+					if(TemplateResources.getProject((IProject)container) != null){
+						containerText.setText(container.getFullPath().toString());
+					}
+				}				
+			}
+		}
+		
+		moduleText.setText("new_module");
+	}
+
+	/**
+	 * Uses the standard container selection dialog to choose the new value for
+	 * the container field.
+	 */
+
+	private void handleBrowse() {
+		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
+				"Select new module container");
+		if (dialog.open() == ContainerSelectionDialog.OK) {
+			Object[] result = dialog.getResult();
+			if (result.length == 1) {
+				containerText.setText(((Path) result[0]).toString());
+			}
+		}
+	}
+
+	/**
+	 * Ensures that both text fields are set.
+	 */
+
+	private void dialogChanged() {
+		IResource container = ResourcesPlugin.getWorkspace().getRoot()
+				.findMember(new Path(getContainerName()));
+		
+		String fileName = getModuleName();
+
+		if (getContainerName().length() == 0) {
+			updateStatus("File container must be specified");
+			return;
+		}
+		if (container == null
+				|| (container.getType() & IResource.PROJECT) == 0) {
+			updateStatus("Module container must exist");
+			return;
+		}
+												
+		if (!container.isAccessible()) {
+			updateStatus("Project must be writable");
+			return;
+		}		
+		if (fileName.length() == 0) {
+			updateStatus("Module name must be specified");
+			return;
+		}
+		if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
+			updateStatus("Module name must be valid");
+			return;
+		}
+		
+		if (container instanceof IProject){
+			TemplateProject project = TemplateResources.getProject((IProject)container);
+			if(project == null){
+				updateStatus("Template Project container must be specified");
+				return;
+			}
+			
+			if(project.getModule(fileName) != null){
+				updateStatus("A module with that name already exists in the project.");
+				return;
+			}				
+		}
+		
+		updateStatus(null);
+	}
+
+	private void updateStatus(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+
+	public String getContainerName() {
+		return containerText.getText();
+	}
+	
+	public IProject getContainerHandle(){
+		return ResourcesPlugin.getWorkspace().getRoot().getProject(
+                getContainerName());
+	}
+	
+	public String getModuleName() {
+		return moduleText.getText();
+	}
+					
+}
