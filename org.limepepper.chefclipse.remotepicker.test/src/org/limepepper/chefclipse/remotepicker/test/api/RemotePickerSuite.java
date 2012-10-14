6a728c94b0a9b50d2966e46ad135114bcfcbccf0
Sebastian Sampaoli <sebastian.sampaoli@mulesoft.com>
2012-10-14 07:29:18 -0300

    Adding a menu item, a handler, and some classes to test a bit of the discovery UI. Besides, a new test plugin is added to test the remote picker api and (maybe) the ui

---
+++ org.limepepper.chefclipse.remotepicker.test/src/org/limepepper/chefclipse/remotepicker/test/api/RemotePickerSuite.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.limepepper.chefclipse.remotepicker.test.api;
+
+import org.junit.Test;
+
+/**
+ * @author Sebastian Sampaoli
+ *
+ */
+public class RemotePickerSuite {
+	
+	@Test
+	public void testSomething() {
+		
+	}
+}
---
+++ org.limepepper.chefclipse.remotepicker.ui/src/main/java/org/limepepper/chefclipse/remotepicker/ui/handlers/RemotePickerHandler.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.limepepper.chefclipse.remotepicker.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.equinox.internal.p2.discovery.Catalog;
+import org.eclipse.equinox.internal.p2.discovery.DiscoveryCore;
+import org.eclipse.equinox.internal.p2.ui.discovery.util.WorkbenchUtil;
+import org.eclipse.equinox.internal.p2.ui.discovery.wizards.CatalogConfiguration;
+import org.eclipse.equinox.internal.p2.ui.discovery.wizards.DiscoveryWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.limepepper.chefclipse.remotepicker.ui.repository.CookbookDiscoveryStrategy;
+
+/**
+ * @author Sebastian Sampaoli
+ *
+ */
+public class RemotePickerHandler extends AbstractHandler {
+
+	/**
+	 * 
+	 */
+	public RemotePickerHandler() {
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		Catalog catalog = new Catalog();
+		catalog.setEnvironment(DiscoveryCore.createEnvironment());
+		catalog.setVerifyUpdateSiteAvailability(false);
+
+		// look for descriptors from installed bundles
+		catalog.getDiscoveryStrategies().add(new CookbookDiscoveryStrategy());
+
+		// look for remote descriptor
+		CookbookDiscoveryStrategy cookbookDiscoveryStrategy = new CookbookDiscoveryStrategy();
+		catalog.getDiscoveryStrategies().add(cookbookDiscoveryStrategy);
+
+		CatalogConfiguration configuration = new CatalogConfiguration();
+		configuration.setShowTagFilter(false);
+
+		DiscoveryWizard wizard = new DiscoveryWizard(catalog, configuration);
+		WizardDialog dialog = new WizardDialog(WorkbenchUtil.getShell(), wizard);
+		try {
+			cookbookDiscoveryStrategy.performDiscovery(null);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		dialog.open();
+		return null;
+	}
+
+}
