9eef05fb47040bd23a8545cc7e9c560aefc0097c
Binhua Liu <binhua.liu1983@gmail.com>
2012-10-22 11:23:57 +0800

    change the graph viewer to editor

---
+++ org.limepepper.chefclipse.graphviewer/src/org/limepepper/chefclipse/graphviewer/actions/OpenDependencyGraphEditorAction.java
@@ -0,0 +1,61 @@
+package org.limepepper.chefclipse.graphviewer.actions;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.limepepper.chefclipse.graphviewer.ui.DependencyGraphEditor;
+import org.limepepper.chefclipse.graphviewer.ui.DependencyGraphEditorInput;
+
+
+public class OpenDependencyGraphEditorAction implements IObjectActionDelegate {
+
+	//private Shell shell;
+	private IWorkbenchPart targetPart;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public OpenDependencyGraphEditorAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		//shell = targetPart.getSite().getShell();
+		this.targetPart=targetPart;
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		try {
+			
+			//PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(GraphView.ID);
+			IStructuredSelection selection = (IStructuredSelection)targetPart.getSite().getWorkbenchWindow().getSelectionService().getSelection();
+			Object firstElement = selection.getFirstElement();
+	        if (firstElement instanceof IAdaptable)
+	        {
+	        	 IProject project = (IProject)((IAdaptable)firstElement).getAdapter(IProject.class);
+	        	 targetPart.getSite().getWorkbenchWindow().getActivePage().openEditor(new DependencyGraphEditorInput(project.getFullPath().toString()), DependencyGraphEditor.ID);
+	        }
+		} catch (PartInitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
---
+++ org.limepepper.chefclipse.graphviewer/src/org/limepepper/chefclipse/graphviewer/ui/DependencyGraphEditorInput.java
@@ -0,0 +1,67 @@
+package org.limepepper.chefclipse.graphviewer.ui;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IPersistableElement;
+
+public class DependencyGraphEditorInput implements IEditorInput {
+
+	private final String projectPath;
+	
+	public String getProjectPath()
+	{
+		return projectPath;
+	}
+	
+	public DependencyGraphEditorInput(String projectPath)
+	{
+		this.projectPath=projectPath;
+	}
+	
+	@Override
+	public Object getAdapter(Class adapter) {
+		return null;
+	}
+
+	@Override
+	public boolean exists() {
+		return true;
+	}
+
+	@Override
+	public ImageDescriptor getImageDescriptor() {
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "Dependency Graph Editor";
+	}
+
+	@Override
+	public IPersistableElement getPersistable() {
+		return null;
+	}
+
+	@Override
+	public String getToolTipText() {
+		return "Dependency Graph Editor";
+	}
+	
+	@Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DependencyGraphEditorInput other = (DependencyGraphEditorInput) obj;
+        if (!projectPath.equals(other.getProjectPath()))
+        {
+            return false;
+        }
+        return true;
+    }
+
+}
