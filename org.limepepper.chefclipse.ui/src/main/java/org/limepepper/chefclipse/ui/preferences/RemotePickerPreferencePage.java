5d1dae612d6e5c1ba99af19cc5e7e3f3ecb13af5
ssampaoli <seba.sampaoli@gmail.com>
2012-11-16 22:08:04 -0300

    Adding a preference page to manage the default cookbook repository.

---
+++ org.limepepper.chefclipse.ui/src/main/java/org/limepepper/chefclipse/ui/preferences/RemotePickerPreferencePage.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.limepepper.chefclipse.ui.preferences;
+
+import java.util.Collection;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.RadioGroupFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.limepepper.chefclipse.remotepicker.api.CookbookRepositoryManager;
+import org.limepepper.chefclipse.remotepicker.api.cookbookrepository.RemoteRepository;
+import org.limepepper.chefclipse.ui.Activator;
+
+/**
+ * @author Sebastian Sampaoli
+ *
+ */
+public class RemotePickerPreferencePage extends FieldEditorPreferencePage
+		implements IWorkbenchPreferencePage {
+
+	public RemotePickerPreferencePage(){
+		super(GRID);
+	}
+	
+	@Override
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+	    setDescription("Set the remote picker general preferences.");
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		
+		String[][] nameAndIdOfRepositories = getRepositoriesInfo(); 
+		RadioGroupFieldEditor editor = new RadioGroupFieldEditor("REPOSITORY SELECTION",
+		        "Select the default repository to be used in the remote picker", 1,
+		        nameAndIdOfRepositories, getFieldEditorParent(), true);
+		addField(editor);
+	}
+
+	private String[][] getRepositoriesInfo() {
+		
+		Collection<RemoteRepository> repositories = CookbookRepositoryManager.getInstance().getRepositories();
+		String[][] repositoriesInfo = new String[repositories.size()][2];
+		int i = 0;
+		for (RemoteRepository remoteRepository : repositories) {			
+			String[] repositoryInfo = {remoteRepository.getName(), remoteRepository.getId()};
+			repositoriesInfo[i++] = repositoryInfo;
+		}
+		return repositoriesInfo;
+	}
+
+}
