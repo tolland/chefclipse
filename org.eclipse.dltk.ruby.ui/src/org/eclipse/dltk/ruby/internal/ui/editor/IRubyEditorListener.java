69090e1014b3431f67170b19dd64d4d0a09abce1
dinkoivanov <dinko.ivanov@gmail.com>
2013-06-14 12:08:05 +0300

    remove patched ruby.ui

--- org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/editor/IRubyEditorListener.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.dltk.ruby.internal.ui.editor;
-
-import org.eclipse.dltk.core.IModelElement;
-
-public interface IRubyEditorListener {
-	void notifyDoSetInput(IModelElement element);
-}
--- org.eclipse.dltk.ruby.ui/src/org/eclipse/dltk/ruby/internal/ui/editor/RubyOutlinePageFactory.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.dltk.ruby.internal.ui.editor;
-
-import org.eclipse.jface.preference.IPreferenceStore;
-
-public class RubyOutlinePageFactory {
-	public RubyOutlinePage createOutlinePage(RubyEditor editor, IPreferenceStore preferenceStore) {
-		return new RubyOutlinePage(editor, preferenceStore);
-	}
-}
